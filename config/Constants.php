b51690ce41b52f65b16d1068b1ea73c772c6e6fc
martinrosbier <martinrosbier@github.com>
2023-06-09 14:54:04 -0300

    #8 composer update and file renaming

--- config/Constants.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace config;
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
- */
-
-define('DEFAULT_NAME', 'plugin-name');
-define('DEFAULT_DIR', 'src/'+DEFAULT_NAME);
