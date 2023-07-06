1a33cd61fcfc7808573a05baa6bd12ae7b8a8638
martinrosbier <martinrosbier@github.com>
2023-07-06 10:23:46 -0300

    description update

--- layout/header.php
+++ layout/header.php
@@ -1,36 +1,48 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="description" content="Martin Rosbier | Full-stack Developer | WordPress Plugin Starter">    
-        <title>WordPress Plugin Starter</title>
-        <link rel="stylesheet" href="vendor/twbs/bootstrap/dist/css/bootstrap.css">
-        <link rel="stylesheet" href="assets/css/styles.css">
-        <link rel="icon" href="assets/img/favicon.ico?2" sizes="32x32" />
-    </head>    
-    
-    <body>
 
-        <div class="container">
-            <div class="row first-row">
-                <div class="col-md-12">
-                    <h1>WordPress Plugin Starter</h1>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Martin Rosbier | Full-stack Developer | WordPress Plugin Starter">
+    <title>WordPress Plugin Starter</title>
+    <link rel="stylesheet" href="vendor/twbs/bootstrap/dist/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="icon" href="assets/img/favicon.ico?2" sizes="32x32" />
+</head>
 
+<body>
+
+    <div class="container">
+        <div class="row first-row">
+            <div class="col-md-12">
+                <h1>WordPress Plugin Starter</h1>
+
+                <p>
+                    This is an open-source project of WordPress, PHP, Bootstrap, and JavaScript.
+                </p>
+
+                <p>
+                    Repository: <a href="https://github.com/martinrosbier/wordpresspluginstarter">github.com/martinrosbier/wordpresspluginstarter</a>
+                </p>
+
+                <div class="alert alert-info">
                     <p>
-                        This is an open-source project of WordPress, PHP, Bootstrap, and JavaScript.
+                        The WordPress Plugin Customizer is a powerful tool designed to simplify the process of creating
+                        custom WordPress plugins.
+                        With this project, you can generate a blank plugin file customized with your plugin's name,
+                        description, author information, and other essential details.
+                        This customizable plugin file serves as a starting point for developing unique WordPress plugins
+                        tailored to your specific needs.
                     </p>
-
                     <p>
-                        Repository: <a href="https://github.com/martinrosbier/wordpresspluginstarter">github.com/martinrosbier/wordpresspluginstarter</a>
+                        This project is built upon the WordPress Plugin Boilerplate, providing a standardized,
+                        organized, and object-oriented foundation for developing high-quality WordPress plugins. It
+                        offers the flexibility to customize the boilerplate according to your needs, automatically
+                        renaming classes and files for seamless integration into your WordPress plugins folder.
                     </p>
-
-                    <div class="alert alert-info">
-                        <p>
-                            This project is built upon the WordPress Plugin Boilerplate, providing a standardized, organized, and object-oriented foundation for developing high-quality WordPress plugins. It offers the flexibility to customize the boilerplate according to your needs, automatically renaming classes and files for seamless integration into your WordPress plugins folder.
-                        </p>
-                    </div>
                 </div>
             </div>
+        </div>
 
-            <div class="row">
+        <div class="row">
